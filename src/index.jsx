549b01d29f10a2d348ca1787ef9bb4d634f34677
macmini <macmini@macminis-Mac-mini.local>
2021-05-07 23:42:46 -0500

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./scss/index.scss";
+import App from "./layout/App";
+import reportWebVitals from "./reportWebVitals";
+
+const routeElement = document.getElementById("root");
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    routeElement
+  );
+}
+
+if (module.hot) {
+  module.hot.accept("./layout/App.jsx", function () {
+    setTimeout(render);
+  });
+}
+
+render();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -1,4 +1,4 @@
-import Navigation from "./components/Navigation/Navigation";
+import Navigation from "../components/Navigation/Navigation";
 
 function App() {
   return (
